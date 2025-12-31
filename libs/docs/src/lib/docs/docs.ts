68e678496b334eccf64f6bca9995384b239b84f1
Warren Dugan <warrendugan@GMAIL.COM>
2025-12-31 14:31:14 -0800

    fix(docs): add missing Icon import

--- libs/docs/src/lib/docs/docs.ts
+++ libs/docs/src/lib/docs/docs.ts
@@ -1,6 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { LogoComponent, ThemeSelectorComponent } from '@basenative/ui/glass';
+import {
+  LogoComponent,
+  ThemeSelectorComponent,
+  Icon,
+} from '@basenative/ui/glass';
 
 @Component({
   selector: 'main[docs-layout]',
@@ -10,6 +14,7 @@ import { LogoComponent, ThemeSelectorComponent } from '@basenative/ui/glass';
     RouterLinkActive,
     LogoComponent,
     ThemeSelectorComponent,
+    Icon,
   ],
   templateUrl: './docs.html',
   styleUrl: './docs.css',
